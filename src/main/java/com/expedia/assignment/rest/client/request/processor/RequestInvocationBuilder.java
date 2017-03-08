4baae20436daf6bbcd7a8ea5143fb25dcbb4f6ca
Mazen Khalil <Mazen Khalil>
2017-03-08 19:53:34 +0200

    commit

--- src/main/java/com/expedia/assignment/rest/client/request/processor/RequestInvocationBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.expedia.assignment.rest.client.request.processor;
-
-import javax.ws.rs.client.Invocation;
-
-public abstract class RequestInvocationBuilder {
-
-	public abstract Invocation build(Invocation.Builder invocationBuilder);
-}
